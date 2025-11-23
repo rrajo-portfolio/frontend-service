7bdd0bf1ce24183ffea1cd99a880fc29af2511b0
roralo <roberto.rajo.lopez@gmail.com>
2025-11-24 00:14:23 +0100

    chore: quitar node_modules del repo

--- dist/frontend-service/browser/runtime-config.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-  window.__PORTFOLIO_CONFIG__ = {
-    apiUrl: 'http://localhost:8085',
-    keycloakUrl: 'http://localhost:7080/auth',
-    keycloakRealm: 'portfolio',
-    keycloakClientId: 'portfolio-frontend'
-  };
-})(window);
-
